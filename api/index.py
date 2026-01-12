9cd55b17f5ff4192d22957a2eba3f702cbe618a5
yezi-zhu <yezi.zhu.work@gmail.com>
2026-01-12 12:50:27 +0800

    Refactor: Move backend into api directory for Vercel bundling

--- api/index.py
+++ api/index.py
@@ -1,7 +1,1 @@
-import sys
-import os
-
-# Add backend directory to sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '../backend'))
-
-from main import app
+from backend.main import app
